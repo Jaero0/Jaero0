cf6fd3a590f680d8c4c9fd17f35078dccfe155ea
rojae1339 <wjdwogh1224@gmail.com>
2025-08-26 12:42:02 +0200

    f

--- index.js
+++ index.js
@@ -69,10 +69,7 @@ const parser = new Parser({
     // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
     for (let i = 0; i < 10; i++) {
         const {title, link} = feed.items[i];
-        console.log(`${i + 1}번째 게시물`);
-        console.log(`추가될 제목: ${title}`);
-        console.log(`추가될 링크: ${link}`);
-        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+        text += `<li><a href="${link}" target="_blank">${title}</a></li>`;
     }
 
     text += `</ul>`;

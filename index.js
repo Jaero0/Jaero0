e438e96086483c8a3371250eba1eee15013e3b48
rojae1339 <wjdwogh1224@gmail.com>
2025-10-25 10:12:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const parser = new Parser({
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL('https://api.velog.io/rss/@wjdwogh1224'); // 본인의 블로그 주소
+    const feed = await parser.parseURL('https://api.velog.io/rss/@hukpak'); // 본인의 블로그 주소
     
     text += `<ul>`;
     

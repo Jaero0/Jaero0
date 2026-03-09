54794c5873f92f43734ced091a62762688121069
rojae1339 <wjdwogh1224@gmail.com>
2026-03-09 10:54:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,19 +21,6 @@ let text = `
 [![My Skills](https://skillicons.dev/icons?i=unity,cs,cpp,git,github)](https://skillicons.dev)
 
 
-## Github Status
-
-<a href="https://github.com/anuraghazra/github-readme-stats">
-    <img src="https://readme-jaero0.vercel.app/api/top-langs/?username=Jaero0&hide=ShaderLab&layout=compact&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true&exclude_repo=Face-Transfer-Application" width=38% />
-</a>
-<a href="https://github.com/anuraghazra/github-readme-stats">
-  <img src="https://readme-jaero0.vercel.app/api?username=Jaero0&show_icons=true&theme=merko&hide_border=true&bg_color=20232a&icon_color=58A6FF&text_color=fff&title_color=58A6FF&count_private=true" width=56% />
-</a>
-
-<a href="https://github.com/ashutosh00710/github-readme-activity-graph">
-    <img src="https://github-readme-activity-graph.vercel.app/graph?username=Jaero0&theme=rogue" width=94%/>
-</a>
-
 ## Latest Blog Posts
 
 `;
